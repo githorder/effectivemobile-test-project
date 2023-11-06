6c26b35943656e0689b3c52b9d14d960774ad045
githorder <mygit.2018@gmail.com>
2023-11-06 11:43:33 +0500

    link postgres server to the project

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const app = require("./app");
+const sequelize = require("./startup/db");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+const startServer = async () => {
+  try {
+    await sequelize.sync({ force: true });
+
+    server.listen(PORT, () => {
+      console.log(`The server is listening on port ${PORT}...`);
+    });
+  } catch (err) {
+    console.log(`Could not start a server due to the following error: `, err);
+  }
+};
+
+startServer();
